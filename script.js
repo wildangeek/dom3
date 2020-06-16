d35dae40e09e3a51a9dc9ddf3fe05730f7f594a3
wildangeek <wildanbeasiswa@gmail.com>
2020-06-16 22:02:08 +0700

    dom3

---
+++ script.js
@@ -0,0 +1,7 @@
+const helloWorld = document.getElementById("hello1");
+console.log(helloWorld);
+
+helloWorld.remove();
+
+const hello2 = document.getElementById("hello2");
+helloName.style.textAlign = "center";
